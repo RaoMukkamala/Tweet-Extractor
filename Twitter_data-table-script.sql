dba444b68931cd1dfd31716d3ec6d94ed86e11a9
RaoMukkamala <mailrao@gmail.com>
2016-05-01 18:45:48 +0200

    Added Twitter Datatable script

---
+++ Twitter_data-table-script.sql
@@ -0,0 +1,51 @@
+﻿
+GO
+
+/****** Object:  Table [dbo].[TweetData]    Script Date: 5/1/2016 6:39:42 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[TweetData](
+	[TweetId] [bigint] NOT NULL,
+	[TweetDate] [datetime] NULL,
+	[Text] [nvarchar](175) NULL,
+	[Truncated] [bit] NULL,
+	[RetweetCount] [smallint] NULL,
+	[FavoriteCount] [smallint] NULL,
+	[Retweeted] [bit] NULL,
+	[Favorited] [bit] NULL,
+	[TweetLang] [varchar](10) NULL,
+	[UserId] [bigint] NULL,
+	[UserName] [nvarchar](100) NULL,
+	[UserLocation] [nvarchar](50) NULL,
+	[UserFollowerCount] [smallint] NULL,
+	[UserFriendsCount] [smallint] NULL,
+	[UserCreatedDate] [datetime] NULL,
+	[UserFavoritesCount] [smallint] NULL,
+	[UserTimeZone] [nvarchar](50) NULL,
+	[UserUTCOffset] [varchar](10) NULL,
+	[UserStatusesCount] [int] NULL,
+	[UserLang] [varchar](10) NULL,
+	[Hashtags] [nvarchar](100) NULL,
+	[RetweetStatusBit] [bit] NULL,
+	[OriginalTweetId] [bigint] NULL,
+	[OriginalTweetUserId] [bigint] NULL,
+	[OriginalTweetDate] [datetime] NULL
+ CONSTRAINT [PK_TweetData] PRIMARY KEY CLUSTERED 
+(
+	[TweetId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
